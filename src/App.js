b08625589691a5752b7ae9ddf9aea2139a12df79
sfburaktrn <sbtorun.sbt@gmail.com>
2023-02-27 22:01:02 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from 'react';
-
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import React from "react";
+import { useReducer } from "react";
+import reducer, { initialState } from "./reducers";
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
+import {
+  addOne,
+  applyNumber,
+  clear,
+  change,
+  memoryEkleme,
+  memoryCagir,
+  memoryClean,
+} from "./actions";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function sayiClick(sayi) {
+    dispatch(applyNumber(sayi));
+  }
+
+  function islemD(islem) {
+    dispatch(change(islem));
+  }
+
+  function C() {
+    dispatch(clear());
+  }
+
+  function memoryC() {
+    dispatch(memoryClean());
+  }
+
+  function memoryRC() {
+    dispatch(memoryCagir());
+  }
+
+  function memoryP() {
+    dispatch(memoryEkleme());
+  }
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -13,47 +49,49 @@ function App() {
       <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation">
+                <b>Operation:</b> {state.operation}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {state.memory}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={"M+"} />
-              <CalcButton value={"MR"} />
-              <CalcButton value={"MC"} />
+              <CalcButton onClick={() => memoryP()} value={"M+"} />
+              <CalcButton onClick={() => memoryRC()} value={"MR"} />
+              <CalcButton onClick={() => memoryC()} value={"MC"} />
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton onClick={() => sayiClick(1)} value={1} />
+              <CalcButton onClick={() => sayiClick(2)} value={2} />
+              <CalcButton onClick={() => sayiClick(3)} value={3} />
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton onClick={() => sayiClick(4)} value={4} />
+              <CalcButton onClick={() => sayiClick(5)} value={5} />
+              <CalcButton onClick={() => sayiClick(6)} value={6} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton onClick={() => sayiClick(7)} value={7} />
+              <CalcButton onClick={() => sayiClick(8)} value={8} />
+              <CalcButton onClick={() => sayiClick(9)} value={9} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton onClick={() => islemD("+")} value={"+"} />
+              <CalcButton onClick={() => islemD("*")} value={"*"} />
+              <CalcButton onClick={() => islemD("-")} value={"-"} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton onClick={() => C()} value={"CE"} />
             </div>
-
           </form>
         </div>
       </div>
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,35 @@
 export const ADD_ONE = "ADD_ONE";
-
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORYE = "MEMORYE";
+export const MEMORYRP = "MEMORYRP";
+export const MEMORYC = "MEMORYC";
 
 export const addOne = () => {
-  return ({ type: ADD_ONE });
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-  return ({ type: APPLY_NUMBER, payload: number });
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+
+export const clear = () => {
+  return { type: CLEAR_DISPLAY, payload: 0 };
+};
+
+export const change = (islem) => {
+  return { type: CHANGE_OPERATION, payload: islem };
+};
+
+export const memoryEkleme = () => {
+  return { type: MEMORYE };
+};
+
+export const memoryCagir = () => {
+  return { type: MEMORYRP };
+};
+
+export const memoryClean = () => {
+  return { type: MEMORYC, payload: 0 };
+};
